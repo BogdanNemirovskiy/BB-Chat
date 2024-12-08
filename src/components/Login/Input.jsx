973d526dc2207435bf56058071288ca8f5db2280
Bohdan Nemyrovskyi <nem.bogdan2006@gmail.com>
2024-12-08 17:52:32 +0100

    added the ability to view your password

--- src/components/Login/Input.jsx
+++ src/components/Login/Input.jsx
@@ -1,17 +1,19 @@
 import React, { useState } from 'react';
 import classes from './Input.module.sass';
+import { Icon } from '@iconify/react/dist/iconify.js';
 
 export default function Input({
-    type = "text",
+    type,
     name,
     placeholder,
     value,
     onChange,
     inputRef,
-    validate,  // New prop for calling validation from the parent
-    error      // New prop for displaying error messages from the parent
+    validate,
+    error
 }) {
     const [isTouched, setIsTouched] = useState(false);
+    const [isPasswordVisible, setIsPasswordVisible] = useState(false);
 
     const handleBlur = () => {
         setIsTouched(true);
@@ -20,18 +22,25 @@ export default function Input({
         }
     };
 
+    const togglePasswordVisibility = () => {
+        setIsPasswordVisible((prevState) => !prevState);
+    };
     return (
         <div className={classes.input__container}>
             <input
-                type={type}
+                type={isPasswordVisible && type === 'password' ? 'text' : type}
                 name={name}
                 placeholder={placeholder}
                 value={value}
                 onChange={onChange}
-                onBlur={handleBlur} // Validate on blur
+                onBlur={handleBlur}
                 ref={inputRef}
                 className={classes.input}
             />
+            {type === 'password' ? <Icon
+                onClick={togglePasswordVisibility}
+                icon={isPasswordVisible ? 'mdi:eye-off' : 'mdi:eye'}
+            /> : null}
             {isTouched && error && <p className={classes.error}>{error}</p>}
         </div>
     );
